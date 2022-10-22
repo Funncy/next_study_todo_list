fd3bfd153ac387ff1e9261350ba1dbef8fb84db7
Funncy <gywns41@naver.com>
2022-10-23 01:52:19 +0900

    [Add] material UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 import TodoItem from "../src/components/todo-item";
+import { Button } from "@mui/material";
 
 const Home: NextPage = () => {
   return (
-   <div className="text-3xl text-stone-200 font-bold underline">
-     TEST
-       <TodoItem/>
-   </div>
-  )
-}
+    <div className="text-3xl text-stone-200 font-bold underline">
+      <Button variant="contained">Contained</Button>
+      <TodoItem />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/components/todo-item.tsx
+++ src/components/todo-item.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React from "react";
+import { BsTrashFill } from "react-icons/bs";
+import { Checkbox } from "@mui/material";
 
+export default function TodoItem() {
+  return (
+    <div className="flex items-center">
+      <Checkbox defaultChecked color="success" />
 
-export default function TodoItem(){
-    return (
-        <div className="text-2xl text-stone-500 font-bold underline">TODO ITEM3</div>
-    );
+      <span className="flex-1 px-2">할 일</span>
+      <div className="px-2 cursor-pointer">
+        <BsTrashFill />
+      </div>
+    </div>
+  );
 }
