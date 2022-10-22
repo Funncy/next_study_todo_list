3f68aaf1a7b14822c0835a39c8b98249fc7d4107
Funncy <gywns41@naver.com>
2022-10-23 02:52:21 +0900

    [Implement] add Todo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,41 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import TodoItem from "../src/components/todo-item";
 import { Button } from "@mui/material";
+import Header from "src/components/header";
+import TodoList from "../src/components/todo-list";
+import InputBox from "../src/components/input-box";
+import { useState } from "react";
+import { todoModel } from "../src/model/todo";
+import uuid from "react-uuid";
 
 const Home: NextPage = () => {
+  const [todos, setTodos] = useState<todoModel[]>([]);
+
+  const addTodo = (title: string) => {
+    setTodos(
+      todos.concat([
+        {
+          id: uuid(),
+          title: title,
+          isDone: false,
+        },
+      ])
+    );
+  };
+
   return (
-    <div className="text-3xl text-stone-200 font-bold underline">
-      <Button variant="contained">Contained</Button>
-      <TodoItem />
+    <div>
+      <div>
+        <Header active={1} />
+        <TodoList todos={todos} />
+        <InputBox
+          onSubmit={(title: string) => {
+            addTodo(title);
+          }}
+        />
+      </div>
     </div>
   );
 };
--- src/components/input-box.tsx
+++ src/components/input-box.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { MouseEventHandler, useState } from "react";
 
 interface IInputBoxProps {
   onSubmit: Function;
 }
 function InputBox({ onSubmit }: IInputBoxProps) {
+  const [title, setTitle] = useState("");
+
+  const handleOnSubmit = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    onSubmit(title);
+    setTitle("");
+  };
+
   return (
     <div className="flex border-solid border-1 rounded-lg h-12 items-center">
       <input
@@ -11,8 +19,15 @@ function InputBox({ onSubmit }: IInputBoxProps) {
         name="title"
         className="flex-1 h-12 pl-4"
         placeholder="Add Todo"
+        value={title}
+        onChange={(event) => {
+          setTitle(event.target.value);
+        }}
       />
-      <div className="w-12 text-white bg-yellow-500 rounded-r-lg px-2 my-auto  h-12 flex items-center">
+      <div
+        onClick={handleOnSubmit}
+        className="w-12 text-white bg-yellow-500 rounded-r-lg px-2 my-auto  h-12 flex items-center"
+      >
         <span>Add</span>
       </div>
     </div>
